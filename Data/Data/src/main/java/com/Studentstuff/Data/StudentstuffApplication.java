ca9da11abd3fa1121d832ae5d29b0adb940e06ef
Nic0Joos <72554161+Nic0Joos@users.noreply.github.com>
2020-10-22 19:21:13 +0200

    Add spring boot framework

---
+++ Data/Data/src/main/java/com/Studentstuff/Data/StudentstuffApplication.java
@@ -0,0 +1,13 @@
+package com.Studentstuff.Data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentstuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentstuffApplication.class, args);
+	}
+
+}
---
+++ Data/Data/src/test/java/com/Studentstuff/Data/StudentstuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Studentstuff.Data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentstuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
