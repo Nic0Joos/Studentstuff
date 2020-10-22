54e69eaa3bfa281d43e5023c9cc135cf87a4d910
Nic0Joos <72554161+Nic0Joos@users.noreply.github.com>
2020-10-22 16:24:02 +0200

    Spring Boot Framework

---
+++ studenstuff/studenstuff/src/test/java/com/studentstuff/studenstuff/StudenstuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentstuff.studenstuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudenstuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
