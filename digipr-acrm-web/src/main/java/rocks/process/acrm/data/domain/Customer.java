b29bd611326e120909dbe4ad85df0adc329303b1
joosn <nico.joos@students.fhnw.ch>
2020-11-17 12:24:50 +0100

    Deleted unnecessary parts

--- digipr-acrm-web/src/main/java/rocks/process/acrm/data/domain/Customer.java
+++ digipr-acrm-web/src/main/java/rocks/process/acrm/data/domain/Customer.java
@@ -1,4 +1,5 @@
-package rocks.process.acrm.data.domain;
+package.com.Studentstuff.data.domain;
+
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -12,56 +13,47 @@ import javax.validation.constraints.NotEmpty;
 @Entity
 public class Customer {
 
-	@Id
-	@GeneratedValue
-	private Long id;
-	private String name;
-	@Email(message = "Please provide a valid e-mail.")
-	@NotEmpty(message = "Please provide an e-mail.")
-	private String email;
-	private String mobile;
-	@ManyToOne
-	@JsonIgnore
-	private Agent agent;
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    @Email(message = "Please provide a valid e-mail.")
+    @NotEmpty(message = "Please provide an e-mail.")
+    private String email;
+    private String mobile;
 
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getMobile() {
-		return mobile;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
+    public String getMobile() {
+        return mobile;
+    }
 
-	public Agent getAgent() {
-		return agent;
-	}
+    public void setMobile(String mobile) {this.mobile = mobile;}
 
-	public void setAgent(Agent agent) {
-		this.agent = agent;
-	}
+}
+
 
 }
