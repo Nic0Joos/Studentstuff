48b047c959ce855d5c4c196274e799e7218308f7
Nic0Joos <72554161+Nic0Joos@users.noreply.github.com>
2020-11-26 14:48:39 +0100

    Added Spring Project

---
+++ Studentstuff-web/Studentstuff-web/src/main/java/rocks/process/Studentstuff/Studentstuffweb/StudentstuffWebApplication.java
@@ -0,0 +1,13 @@
+package rocks.process.Studentstuff.Studentstuffweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentstuffWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentstuffWebApplication.class, args);
+	}
+
+}
