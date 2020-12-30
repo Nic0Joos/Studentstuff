c288a78cbce0a100bc05bfad229817f022e81b3f
joosn <nico.joos@students.fhnw.ch>
2020-12-30 14:52:29 +0100

    getCookie method

--- Studentstuff-web/Studentstuff-web/src/main/resources/StudentStuff Webpage/assets/js/app.js
+++ Studentstuff-web/Studentstuff-web/src/main/resources/StudentStuff Webpage/assets/js/app.js
@@ -52,3 +52,8 @@ function getOfferJSON(id, name, price, description, files) {
         });
     }
 }
+
+function getCookie(name) {
+    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) return match[2];
+}
