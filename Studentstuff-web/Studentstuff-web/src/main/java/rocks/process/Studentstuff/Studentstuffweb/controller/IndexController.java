e23f3b6faa2dcb69e1da197d9e7818f9b9d033a3
joosn <nico.joos@students.fhnw.ch>
2021-01-12 16:29:23 +0100

    Rollback

--- Studentstuff-web/Studentstuff-web/src/main/java/rocks/process/Studentstuff/Studentstuffweb/controller/IndexController.java
+++ Studentstuff-web/Studentstuff-web/src/main/java/rocks/process/Studentstuff/Studentstuffweb/controller/IndexController.java
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
 package rocks.process.Studentstuff.Studentstuffweb.controller;
 
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-    @RequestMapping(path = "/")
-    public class IndexController {
-
-
-        @GetMapping
-        public String getIndexView() {
-            return "Home.html";
-        }
-
-        @GetMapping(path = "/Policy")
-        public String getPolicyView() {
-            return "PrivacyPolicy.html";
-        }
-    }
-
-=======
 @Controller
 @RequestMapping (path = "/")
-public class CustomerController {
-    @getMapping
-    public String getIndexView() {return "Home.html"}
+public class IndexController {
+
+    @GetMapping
+    public String getIndexView() {return "Home.html";}
+
 }
->>>>>>> parent of 7d7e3dd... Merge pull request #20 from andreasmartin/main
