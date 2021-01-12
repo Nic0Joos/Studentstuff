215ded57ff9bcdaa2a947e81d025150376b1f0f7
joosn <nico.joos@students.fhnw.ch>
2021-01-12 19:20:06 +0100

    Index file

--- src/main/java/rocks/process/Studentstuff/Studentstuffweb/controller/IndexController.java
+++ src/main/java/rocks/process/Studentstuff/Studentstuffweb/controller/IndexController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @GetMapping
-    public String getIndexView() {return "Home.html";}
+    public String getIndexView() {return "Index.html";}
 
 }
